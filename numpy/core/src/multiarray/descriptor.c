b4671853c5fe9a5fa1e2162f3972efd1602daa95
Eric Wieser <wieser.eric@gmail.com>
2017-04-21 10:52:57 +0100

    ENH: Make .itemsize return none when .elsize == -1

--- numpy/core/src/multiarray/descriptor.c
+++ numpy/core/src/multiarray/descriptor.c
@@ -1804,8 +1804,6 @@ static PyMemberDef arraydescr_members[] = {
         T_INT, offsetof(PyArray_Descr, type_num), READONLY, NULL},
     {"byteorder",
         T_CHAR, offsetof(PyArray_Descr, byteorder), READONLY, NULL},
-    {"itemsize",
-        T_INT, offsetof(PyArray_Descr, elsize), READONLY, NULL},
     {"alignment",
         T_INT, offsetof(PyArray_Descr, alignment), READONLY, NULL},
     {"flags",
@@ -1977,6 +1975,14 @@ arraydescr_ndim_get(PyArray_Descr *self)
     return PyInt_FromLong(1);
 }
 
+static PyObject *
+arraydescr_itemsize_get(PyArray_Descr *self)
+{
+    if (self->elsize == -1) {
+        Py_RETURN_NONE;
+    }
+    return PyInt_FromLong(self->elsize);
+}
 
 NPY_NO_EXPORT PyObject *
 arraydescr_protocol_descr_get(PyArray_Descr *self)
@@ -2260,6 +2266,9 @@ static PyGetSetDef arraydescr_getsets[] = {
         NULL, NULL},
     {"hasobject",
         (getter)arraydescr_hasobject_get,
+        NULL, NULL, NULL},
+    {"itemsize",
+        (getter)arraydescr_itemsize_get,
         NULL, NULL, NULL},
     {NULL, NULL, NULL, NULL, NULL},
 };
