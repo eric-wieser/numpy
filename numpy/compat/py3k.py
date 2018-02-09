d2055773900d4535f7d046836bf84ae0fb1787b1
Eric Wieser <wieser.eric@gmail.com>
2018-02-08 21:40:21 -0800

    MAINT: Close the file if any unexpected errors occur within memmap

--- numpy/compat/py3k.py
+++ numpy/compat/py3k.py
@@ -97,6 +97,28 @@ def is_pathlib_path(obj):
     """
     return Path is not None and isinstance(obj, Path)
 
+# from Python 3.7
+class contextlib_nullcontext(object):
+    """Context manager that does no additional processing.
+
+    Used as a stand-in for a normal context manager, when a particular
+    block of code is only sometimes used with a normal context manager:
+
+    cm = optional_cm if condition else nullcontext()
+    with cm:
+        # Perform operation, using optional_cm if condition is True
+    """
+
+    def __init__(self, enter_result=None):
+        self.enter_result = enter_result
+
+    def __enter__(self):
+        return self.enter_result
+
+    def __exit__(self, *excinfo):
+        pass
+
+
 if sys.version_info[0] >= 3 and sys.version_info[1] >= 4:
     def npy_load_module(name, fn, info=None):
         """
