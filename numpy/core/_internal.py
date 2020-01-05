13472e6e1efc2592f3042e5442f60507d8d73a35
Eric Wieser <wieser.eric@gmail.com>
2020-01-05 21:43:01 +0000

    MAINT: Eliminate some calls to `eval`
    * The instance in `_internal` is parsing tuples and integers, so `ast.literal_eval` is just fine
    * The instance in `crack_fortran` is just trying to lookup a function name dynamically
    * The instance in `f90mod_rules` is looking at the result of `capi_maps.getarrdims(...)['rank']`, which is always a string representation of an integer
    
    The f2py code is still littered with `eval`, but the remaining cases were harder to reason about.

--- numpy/core/_internal.py
+++ numpy/core/_internal.py
@@ -4,6 +4,7 @@ A place for internal code
 Some things are more easily handled Python.
 
 """
+import ast
 import re
 import sys
 import platform
@@ -196,7 +197,7 @@ def _commastring(astr):
         if (repeats == b''):
             newitem = dtype
         else:
-            newitem = (dtype, eval(repeats))
+            newitem = (dtype, ast.literal_eval(repeats.decode('ascii')))
         result.append(newitem)
 
     return result
